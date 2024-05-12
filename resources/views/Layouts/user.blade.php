942b002108987bffa906d99ef749d722cf2207db
Iksan273 <iksan8991@gmail.com>
2024-05-12 21:59:10 +0700

    Setup Laravel 10 + Starter Template

---
+++ resources/views/Layouts/user.blade.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Dasho - Bootstrap 5 Admin Template</title>
+    <!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 11]>
+    	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    	<![endif]-->
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description"
+    	content="Dasho Bootstrap admin template made using Bootstrap 5 and it has huge amount of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
+    <meta name="keywords"
+    	content="admin templates, bootstrap admin templates, bootstrap 5, dashboard, dashboard templets, sass admin templets, html admin templates, responsive, bootstrap admin templates free download,premium bootstrap admin templates, Dasho, Dasho bootstrap admin template">
+    <meta name="author" content="Phoenixcoded" />
+
+    <!-- Favicon icon -->
+    <link rel="icon" href="assets/images/favicon.svg" type="image/x-icon">
+    <!-- fontawesome icon -->
+    <link rel="stylesheet" href="assets/fonts/fontawesome/css/fontawesome-all.min.css">
+    <!-- animation css -->
+    <link rel="stylesheet" href="assets/plugins/animation/css/animate.min.css">
+    <!-- vendor css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body class="">
+	<!-- [ Pre-loader ] start -->
+	<div class="loader-bg">
+		<div class="loader-track">
+			<div class="loader-fill"></div>
+		</div>
+	</div>
+	<!-- [ Pre-loader ] End -->
+	<!-- [ navigation menu ] start -->
+	<nav class="pcoded-navbar menupos-fixed menu-dark menu-item-icon-style6 ">
+		<div
+			class="navbar-wrapper ">
+			<div class="navbar-brand header-logo">
+				<a href="index.html" class="b-brand">
+
+					<img src="assets/images/logo.svg" alt="logo" class="logo images">
+					<img src="assets/images/logo-icon.svg" alt="logo" class="logo-thumb images">
+				</a>
+				<a class="mobile-menu" id="mobile-collapse" href="#!"><span></span></a>
+			</div>
+			<div class="navbar-content scroll-div   "
+				id="layout-sidenav" >
+				<ul class="nav pcoded-inner-navbar sidenav-inner">
+					<li class="nav-item pcoded-menu-caption">
+						<label>Navigation</label>
+					</li>
+					<li data-username="dashboard default ecommerce sales Helpdesk ticket CRM analytics project"
+						class="nav-item pcoded-hasmenu">
+						<a href="#!" class="nav-link"><span class="pcoded-micon"><i class="feather icon-home"></i></span><span
+								class="pcoded-mtext">Dashboard</span></a>
+						<ul class="pcoded-submenu">
+							<li class=""><a href="index.html" class="">Analytics</a></li>
+							<li class=""><a href="dashboard-sale.html" class="">Sales</a></li>
+							<li class=""><a href="dashboard-project.html" class="">Project</a></li>
+							<li class=""><a href="dashboard-help.html" class="">Helpdesk<span
+										class="pcoded-badge label label-danger">NEW</span></a></li>
+						</ul>
+					</li>
+					<li data-username="vertical horizontal box layout RTL fixed static collapse menu color icon dark background image"
+						class="nav-item pcoded-hasmenu">
+						<a href="#!" class="nav-link"><span class="pcoded-micon"><i class="feather icon-layout"></i></span><span
+								class="pcoded-mtext">Page layouts</span></a>
+						<ul class="pcoded-submenu">
+							<li class="pcoded-hasmenu"><a href="#!" class="">Vertical</a>
+								<ul class="pcoded-submenu">
+									<li class=""><a href="layout-static.html" class="" target="_blank">Static</a></li>
+									<li class=""><a href="layout-fixed.html" class="" target="_blank">Fixed</a></li>
+									<li class=""><a href="layout-menu-fixed.html" class="" target="_blank">Navbar fixed</a></li>
+									<li class=""><a href="layout-mini-menu.html" class="" target="_blank">Collapse menu</a></li>
+									<li class=""><a href="layout-navbg.html" class="" target="_blank">Navbar imagebg</a></li>
+								</ul>
+							</li>
+							<li class=""><a href="layout-horizontal.html" class="" target="_blank">Horizontal</a></li>
+							<li class=""><a href="layout-horizontal-1.html" class="" target="_blank">Horizontal v1</a></li>
+							<li class=""><a href="layout-horizontal-2.html" class="" target="_blank">Horizontal v2</a></li>
+							<li class=""><a href="layout-horizontal-rtl.html" class="" target="_blank">Horizontal RTL</a></li>
+							<li class=""><a href="layout-box.html" class="" target="_blank">Box layout</a></li>
+							<li class=""><a href="layout-rtl.html" class="" target="_blank">RTL</a></li>
+							<li class=""><a href="layout-light.html" class="" target="_blank">Light layout</a></li>
+							<li class=""><a href="layout-dark.html" class="" target="_blank">Dark layout <span
+										class="pcoded-badge label label-danger">Hot</span></a></li>
+							<li class=""><a href="layout-menu-icon.html" class="" target="_blank">Color icon</a></li>
+						</ul>
+					</li>
+					<li data-username="widget statistic data chart" class="nav-item pcoded-hasmenu">
+						<a href="#!" class="nav-link"><span class="pcoded-micon"><i class="feather icon-layers"></i></span><span
+								class="pcoded-mtext">Widget</span><span class="pcoded-badge label label-success">100+</span></a>
+						<ul class="pcoded-submenu">
+							<li class=""><a href="widget-statistic.html" class="">Statistic</a></li>
+							<li class=""><a href="widget-data.html" class="">Data</a></li>
+							<li class=""><a href="widget-chart.html" class="">Chart</a></li>
+						</ul>
+					</li>
+
+				</ul>
+
+
+
+			</div>
+
+		</div>
+	</nav>
+	<!-- [ navigation menu ] end -->
+
+
+
+	<!-- [ Header ] start -->
+	<header class="navbar pcoded-header navbar-expand-lg navbar-light headerpos-fixed">
+			<div class="m-header">
+				<a class="mobile-menu" id="mobile-collapse1" href="#!"><span></span></a>
+				<a href="index.html" class="b-brand">
+
+					<img src="assets/images/logo.svg" alt="" class="logo images">
+					<img src="assets/images/logo-icon.svg" alt="" class="logo-thumb images">
+				</a>
+			</div>
+			<a class="mobile-menu" id="mobile-header" href="#!">
+				<i class="feather icon-more-horizontal"></i>
+			</a>
+			<div class="collapse navbar-collapse">
+				<a href="#!" class="mob-toggler"></a>
+				<ul class="navbar-nav me-auto">
+					<li class="nav-item">
+						<div class="main-search open">
+							<div class="input-group">
+								<input type="text" id="m-search" class="form-control" placeholder="Search . . .">
+								<a href="#!" class="input-group-append search-close">
+									<i class="feather icon-x input-group-text"></i>
+								</a>
+								<span class="ms-1 input-group-append search-btn btn btn-primary">
+									<i class="feather icon-search input-group-text"></i>
+								</span>
+							</div>
+						</div>
+					</li>
+				</ul>
+
+			</div>
+
+	</header>
+
+
+<!-- [ Main Content ] start -->
+<div class="pcoded-main-container">
+    <div class="pcoded-wrapper">
+        @yield('content')
+
+    </div>
+</div>
+
+
+<!-- Required Js -->
+<script src="assets/js/vendor-all.min.js"></script>
+<script src="assets/plugins/bootstrap/js/popper.min.js"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/pcoded.min.js"></script>
+
+
+<!-- am chart js -->
+<script src="assets/plugins/chart-am4/js/core.js"></script>
+<script src="assets/plugins/chart-am4/js/charts.js"></script>
+<script src="assets/plugins/chart-am4/js/animated.js"></script>
+<script src="assets/plugins/chart-am4/js/maps.js"></script>
+<script src="assets/plugins/chart-am4/js/worldLow.js"></script>
+<script src="assets/plugins/chart-am4/js/continentsLow.js"></script>
+
+<!-- dashboard-custom js -->
+<script src="assets/js/pages/dashboard-sale.js"></script>
+
+
+
+
+
+</body>
+
+</html>
